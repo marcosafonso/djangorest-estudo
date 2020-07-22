47f6f02f23c06d004b38e582716d5fe0bb06e733
Marcos Afonso <afonso-vieira@hotmail.com>
2020-07-22 16:59:45 -0300

    rest vue docker

--- restvue/settings.py
+++ restvue/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'restvue.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'restvuedb',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
