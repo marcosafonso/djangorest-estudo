42b5ee69d9036c1c1e8852819d6f6813839f224d
Marcos Afonso <afonso-vieira@hotmail.com>
2020-07-24 13:59:12 -0300

    django rest meu curso

--- restvue/settings.py
+++ restvue/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'on_pd_02*7-vjdv2+=wz%7f7g&=r^2^hxh_z8urw)evkag@8s='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.2']
+ALLOWED_HOSTS = ['127.0.0.1','localhost', '127.0.0.2']
 
 
 # Application definition
@@ -82,14 +82,21 @@ WSGI_APPLICATION = 'restvue.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'restvuedb',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'restvuedb',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': '127.0.0.1',
+#         'PORT': 5432,
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
